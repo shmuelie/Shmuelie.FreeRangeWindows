cf8424d52cc42d3c09fd82b62a48268928bf0e0c
Shmueli Englard <shmueli.yosef@englard.net>
2018-01-10 13:28:44 -0500

    Added suppressing more

--- GlobalSuppressions.cs
+++ GlobalSuppressions.cs
@@ -5,9 +5,17 @@
 // a specific target and scoped to a namespace, type, member, etc.
 
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "CC0001:You should use 'var' whenever possible.", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "RCS1010", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "RECS0091", Justification = "<Pending>")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "RECS0145", Justification = "<Pending>")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = nameof(Shmuelie), Scope = "namespace", Target = "Shmuelie.FreeRangeWindows")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA1704:IdentifiersShouldBeSpelledCorrectly", MessageId = nameof(Shmuelie))]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "ViewManager", Scope = "member", Target = "Shmuelie.FreeRangeWindows.FreeRangeWindowsPackage.#Initialize()")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "FloatingWindow", Scope = "member", Target = "Shmuelie.FreeRangeWindows.FreeRangeWindowsPackage.#Initialize()")]
 [assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Naming", "CA2204:Literals should be spelled correctly", MessageId = "ChangeOwner", Scope = "member", Target = "Shmuelie.FreeRangeWindows.FreeRangeWindowsPackage.#CreateChangeOwner()")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Microsoft.VisualStudio.Shell.ActivityLog.TryLogWarning(System.String,System.String)", Scope = "member", Target = "Shmuelie.FreeRangeWindows.FreeRangeWindowsPackage.#Initialize()")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Microsoft.VisualStudio.Shell.ActivityLog.LogInformation(System.String,System.String)", Scope = "member", Target = "Shmuelie.FreeRangeWindows.FreeRangeWindowsPackage.#Initialize()")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Microsoft.VisualStudio.Shell.ActivityLog.TryLogWarning(System.String,System.String)", Scope = "member", Target = "Shmuelie.FreeRangeWindows.FreeRangeWindowsPackage.#CreateChangeOwner()")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Microsoft.VisualStudio.Shell.ActivityLog.LogInformation(System.String,System.String)", Scope = "member", Target = "Shmuelie.FreeRangeWindows.FreeRangeWindowsPackage.#CreateChangeOwner()")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Microsoft.VisualStudio.Shell.ActivityLog.LogInformation(System.String,System.String)", Scope = "member", Target = "Shmuelie.FreeRangeWindows.FreeRangeWindowsPackage.#FloatingWindowVisibilityChanged(System.Windows.DependencyObject,System.Windows.DependencyPropertyChangedEventArgs)")]
 
