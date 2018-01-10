9b87b057986bc2d01978fa7109974a0ab23238c7
Shmueli Englard <shmueli.yosef@englard.net>
2018-01-10 13:31:10 -0500

    Changed to not use reflection and now window title changes to reflect tool window

--- FreeRangeWindowsPackage.cs
+++ FreeRangeWindowsPackage.cs
@@ -1,10 +1,10 @@
 ﻿using System;
-using System.Diagnostics.CodeAnalysis;
-using System.Reflection;
-using System.Reflection.Emit;
 using System.Runtime.InteropServices;
 using System.Windows;
 using System.Windows.Data;
+using Microsoft.VisualStudio.Platform.WindowManagement;
+using Microsoft.VisualStudio.PlatformUI.Shell;
+using Microsoft.VisualStudio.PlatformUI.Shell.Controls;
 using Microsoft.VisualStudio.Shell;
 using Microsoft.VisualStudio.Shell.Interop;
 
@@ -23,68 +23,66 @@ namespace Shmuelie.FreeRangeWindows
         /// </summary>
         public const string PackageGuidString = "214337a7-2957-45e5-b42e-b8cf91cbe11f";
 
-        [SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Microsoft.VisualStudio.Shell.ActivityLog.LogInformation(System.String,System.String)")]
-        [SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Microsoft.VisualStudio.Shell.ActivityLog.TryLogWarning(System.String,System.String)")]
         protected override void Initialize()
         {
             base.Initialize();
-            Assembly viewManagerAssembly = Assembly.Load("Microsoft.VisualStudio.Shell.ViewManager");
-            if (viewManagerAssembly == null)
-            {
-                ActivityLog.TryLogWarning(nameof(FreeRangeWindows), "Unable to load ViewManager");
-                return;
-            }
-            Type floatingWindowType = viewManagerAssembly.GetType("Microsoft.VisualStudio.PlatformUI.Shell.Controls.FloatingWindow", false);
-            if (floatingWindowType == null)
-            {
-                ActivityLog.TryLogWarning(nameof(FreeRangeWindows), "Unable to find FloatingWindow");
-                return;
-            }
             ActivityLog.LogInformation(nameof(FreeRangeWindows), "Registering for FloatingWindow changes");
-            UIElement.VisibilityProperty.OverrideMetadata(floatingWindowType, new FrameworkPropertyMetadata(Visibility.Collapsed, FloatingWindowVisibilityChanged));
-        }
-
-        private readonly static Lazy<Func<Window, bool>> lazyChangeOwner = new Lazy<Func<Window, bool>>(CreateChangeOwner, true);
-
-        private static bool Empty(Window window) => false;
-
-        [SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Microsoft.VisualStudio.Shell.ActivityLog.LogInformation(System.String,System.String)")]
-        [SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Microsoft.VisualStudio.Shell.ActivityLog.TryLogWarning(System.String,System.String)")]
-        private static Func<Window, bool> CreateChangeOwner()
-        {
-            Type floatingWindowType = Assembly.Load("Microsoft.VisualStudio.Shell.ViewManager").GetType("Microsoft.VisualStudio.PlatformUI.Shell.Controls.FloatingWindow", false);
-            MethodInfo changeOwnerInfo = floatingWindowType.GetMethod("ChangeOwner", BindingFlags.Instance | BindingFlags.InvokeMethod | BindingFlags.NonPublic | BindingFlags.Public, null, new[] { typeof(IntPtr) }, null);
-            if (changeOwnerInfo == null)
-            {
-                ActivityLog.TryLogWarning(nameof(FreeRangeWindows), "Unable to find ChangeOwner");
-                return Empty;
-            }
-            DynamicMethod dynamicChangeOwner = new DynamicMethod(changeOwnerInfo.Name, typeof(bool), new[] { typeof(Window) }, true);
-            ILGenerator generator = dynamicChangeOwner.GetILGenerator();
-            generator.Emit(OpCodes.Ldarg_0);
-            generator.Emit(OpCodes.Castclass, floatingWindowType);
-            generator.Emit(OpCodes.Ldsfld, typeof(IntPtr).GetField(nameof(IntPtr.Zero)));
-            generator.Emit(OpCodes.Call, changeOwnerInfo);
-            generator.Emit(OpCodes.Ldc_I4_1);
-            generator.Emit(OpCodes.Ret);
-            ActivityLog.LogInformation(nameof(FreeRangeWindows), "Created ChangeOwner wrapper");
-            return (Func<Window, bool>)dynamicChangeOwner.CreateDelegate(typeof(Func<Window, bool>));
+            UIElement.VisibilityProperty.OverrideMetadata(typeof(FloatingWindow), new FrameworkPropertyMetadata(Visibility.Collapsed, FloatingWindowVisibilityChanged));
+            ViewElement.IsSelectedProperty.OverrideMetadata(typeof(View), new PropertyMetadata(false, ViewElementBooleanDependencyPropertyChanged));
+            ViewElement.IsSelectedProperty.OverrideMetadata(typeof(ToolWindowView), new PropertyMetadata(false, ViewElementBooleanDependencyPropertyChanged));
+            View.IsActiveProperty.OverrideMetadata(typeof(ToolWindowView), new FrameworkPropertyMetadata(false, ViewElementBooleanDependencyPropertyChanged));
         }
 
 #pragma warning disable U2U1003 // Avoid declaring methods used in delegate constructors static
-        [SuppressMessage("Microsoft.Globalization", "CA1303:Do not pass literals as localized parameters", MessageId = "Microsoft.VisualStudio.Shell.ActivityLog.LogInformation(System.String,System.String)")]
+        private static void ViewElementBooleanDependencyPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+#pragma warning restore U2U1003 // Avoid declaring methods used in delegate constructors static
+        {
+            if ((bool)e.NewValue && d != null && d is View view && view.Content != null)
+            {
+                UIElement content = (UIElement)view.Content;
+                Window window = Window.GetWindow(content);
+                if (window != null)
+                {
+                    BindTitle(window, view);
+                }
+                else
+                {
+                    void isVisibleChanged(object sender, DependencyPropertyChangedEventArgs args)
+                    {
+                        UIElement element = (UIElement)sender;
+                        if ((bool)args.NewValue)
+                        {
+                            BindTitle(Window.GetWindow(element), view);
+                        }
+                        element.IsVisibleChanged -= isVisibleChanged;
+                    }
+                    content.IsVisibleChanged += isVisibleChanged;
+                }
+            }
+        }
+
+        private static void BindTitle(Window window, View view)
+        {
+            string mainTitle = Application.Current.MainWindow.Title;
+            window.SetBinding(Window.TitleProperty, new Binding("Title")
+            {
+                FallbackValue = mainTitle,
+                Mode = BindingMode.OneWay,
+                StringFormat = mainTitle + " - {0}",
+                Source = view
+            });
+        }
+
+#pragma warning disable U2U1003 // Avoid declaring methods used in delegate constructors static
         private static void FloatingWindowVisibilityChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
 #pragma warning restore U2U1003 // Avoid declaring methods used in delegate constructors static
         {
             if ((Visibility)e.NewValue == Visibility.Visible)
             {
-                Window window = (Window)d;
-                if (lazyChangeOwner.Value(window))
-                {
-                    ActivityLog.LogInformation(nameof(FreeRangeWindows), "Making " + window.Title + " free range");
-                    window.ShowInTaskbar = true;
-                    BindingOperations.SetBinding(window, Window.TitleProperty, new Binding("DataContext.Child.SelectedElement.Title.Title") { Source = window });
-                }
+                FloatingWindow window = (FloatingWindow)d;
+                window.ChangeOwner(IntPtr.Zero);
+                window.ShowInTaskbar = true;
+                ActivityLog.LogInformation(nameof(FreeRangeWindows), "Making " + window.Title + " free range");
             }
         }
     }
